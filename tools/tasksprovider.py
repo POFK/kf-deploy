47fe4af77532458ed0242119afce2c0838b8b135
POFK <mtianxiang@gmail.com>
2021-06-08 14:23:18 +0800

    ci(workflow): demo for matrix from python script

---
+++ tools/tasksprovider.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import yaml, json
+
+#path = "/config/workspace/learnk8s/kubeflow/kf-test/.cache/manifests/manifests-1.2-branch/gcp/iap-ingress/v3/kustomization.yaml"
+#with open(path, 'r') as stream:
+#    try:
+#        data = yaml.safe_load(stream)
+#    except yaml.YAMLError as exc:
+#        print(exc)
+#
+#pprint.pprint(data['images'])
+#
+#
+#pprint.pprint(json.dumps(data['images']))
+
+Data = {"include": []}
+for i in range(3):
+    data = {}
+    data['src'] = i
+    data['des'] = i * 10
+    Data["include"].append(data)
+output = json.dumps(Data)
+pre = "::set-output name=matrix::"
+print(pre+output)
